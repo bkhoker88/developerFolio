9a45fe104fea6871262a97ed8e190933f6ae3db9
bkhoker88 <70544344+bkhoker88@users.noreply.github.com>
2021-05-12 20:44:24 +0000

    Deploying to gh-pages from  @ f4102f09a7248c51facbd60cfa1ee3dc03c8e84d 🚀

---
+++ precache-manifest.1cbf12217d5be55ba1c7f33ef3674a62.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b4deb01f4caecb7a514d11d37e10059",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df5b6cf1a4809e7007cc",
+    "url": "/static/css/3.6a352120.chunk.css"
+  },
+  {
+    "revision": "257ce006e2ddf714cd4f",
+    "url": "/static/css/4.6e14a8dd.chunk.css"
+  },
+  {
+    "revision": "b871f12dd451145560a8",
+    "url": "/static/css/main.4421ce3d.chunk.css"
+  },
+  {
+    "revision": "4514e786a79b53413a35",
+    "url": "/static/js/2.21e0061b.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.21e0061b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df5b6cf1a4809e7007cc",
+    "url": "/static/js/3.a3d01360.chunk.js"
+  },
+  {
+    "revision": "257ce006e2ddf714cd4f",
+    "url": "/static/js/4.74a80a00.chunk.js"
+  },
+  {
+    "revision": "b871f12dd451145560a8",
+    "url": "/static/js/main.b70b20ec.chunk.js"
+  },
+  {
+    "revision": "aa5b3eb4067ab00626ed",
+    "url": "/static/js/runtime-main.1ea81a0c.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "772aec7693fa2f62afbbc184df10b86a",
+    "url": "/static/media/EBS.772aec76.jpg"
+  },
+  {
+    "revision": "5f1f36a5ce3417a41f584cbdf8827405",
+    "url": "/static/media/Fullstack.5f1f36a5.png"
+  },
+  {
+    "revision": "64ce94288cb06e2685dba649c9dee90f",
+    "url": "/static/media/Memeazon.64ce9428.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "7a4917be38fbaa1e00ddcd2d03e53f0d",
+    "url": "/static/media/outerview.7a4917be.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bf909001a9a77ef82c675d0e4ea35eda.js"
+  "/precache-manifest.1cbf12217d5be55ba1c7f33ef3674a62.js"
 );
 
 self.addEventListener('message', (event) => {
